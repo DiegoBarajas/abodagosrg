445e8804194ec2af2766151ea539d2cf94e724e4
DiegoBarajas <desaubv@gmail.com>
2023-10-22 01:27:24 -0600

    Work in progress

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+const App = () => {
+    return (
+      <div>
+        <h1 className='work-in-progress-h1'>RG y Asociados</h1>
+        <p className='work-in-progress-p'>Trabajo en proceso</p>
+      </div>
+    )
+}
+
+export default App
